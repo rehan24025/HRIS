acaaabd6b4b6216e387cbfdfc7bd237ed84803f2
SyabilIqmaulana <165236934+SyabilIqmaulana@users.noreply.github.com>
2026-01-08 11:02:16 +0700

    tambah database migrate

---
+++ database/migrations/2026_01_08_034102_create_pelanggaran_karyawan_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+       Schema::create('pelanggaran_karyawan', function (Blueprint $table) {
+    $table->id();
+    $table->foreignId('karyawan_id')->constrained('karyawan')->cascadeOnDelete();
+    $table->foreignId('jenis_pelanggaran_id')->constrained('jenis_pelanggaran')->cascadeOnDelete();
+    $table->date('tanggal');
+    $table->text('catatan')->nullable();
+    $table->timestamps();
+});
+
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pelanggaran_karyawan');
+    }
+};
